5070025f50047a2622ee9f68a314eab63d99acba
Bartolg <bartol.gdula@gmail.com>
2025-09-28 23:07:28 +0100

    Enable immersive fullscreen mode

--- app/src/main/java/com/pykens/earthzoo/MainActivity.kt
+++ app/src/main/java/com/pykens/earthzoo/MainActivity.kt
@@ -3,6 +3,9 @@ package com.pykens.earthzoo
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.pykens.earthzoo.ui.InteractiveEarthView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
 
         val selectedContinentLabel: TextView = findViewById(R.id.selected_continent_label)
         val earthView: InteractiveEarthView = findViewById(R.id.interactive_earth)
